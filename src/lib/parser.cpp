38d55c6ce8e2d0fcb5ee825e005618fc06a9f89d
Leo Zhu <leozhu@ucsb.edu>
2023-10-15 16:07:56 -0700

    fixed constructor

--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -10,7 +10,7 @@ Parser::~Parser() {
 
 Parser::Parser(std::vector<Token> tokens) {
   if (tokens.size() == 0) {
-    cout << "No tokens" << endl;
+    std::cout << "No tokens" << endl;
     exit(2);
   }
 
