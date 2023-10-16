95017d88f29e1a8c5c04834e52b177945db42759
Leo Zhu <leozhu@ucsb.edu>
2023-10-15 23:58:03 -0700

    new line?

--- src/parse.cpp
+++ src/parse.cpp
@@ -8,7 +8,7 @@ int main() {
 
   Parser parser = Parser(lexer.lexer());
 
-  std::cout << parser.toString() << parser.calculate();
+  std::cout << parser.toString() << std::endl << parser.calculate() << "\n";
 	  
   return 0;
 }
