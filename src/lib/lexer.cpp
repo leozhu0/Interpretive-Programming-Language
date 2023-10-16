638276ed4e0823b152acefb40181761439d8a56b
John Lins <johnpaullins@gmail.com>
2023-10-15 22:18:20 -0700

    needs two returns

--- src/lib/lexer.cpp
+++ src/lib/lexer.cpp
@@ -50,14 +50,18 @@ std::vector<Token> Lexer::lexer(){
     std::vector<Token> sequence;
     std::string raw_input;
     int line = 1;
-   // bool extra_line = false;
+    bool extra_line = false;
     //std::istringstream alllines(input);
-    while (std::getline(std::cin, raw_input)) {
-        // if(raw_input == ""){
-        //     extra_line = true;
-        // }else{
-        //     extra_line= false;
-        // }
+    while (true) {
+        std::getline(std::cin, raw_input);
+
+        if(raw_input == "" && extra_line){
+            break;
+        } else if (raw_input == ""){
+             extra_line = true;
+        } else {
+            extra_line = false;
+        }
 
         int num_decimal = 0;
         std::string element = "";
@@ -98,11 +102,11 @@ std::vector<Token> Lexer::lexer(){
         line++;
     }
 
-    //if(extra_line){
-    //    sequence.push_back(Token{line,1,"END", END});
-    //}else {
+    if(extra_line){
+        sequence.push_back(Token{line,1,"END", END});
+    }else {
         sequence.push_back(Token{line-1,sequence.back().column + 1,"END", END});
-    //}
+    }
     
 
 
