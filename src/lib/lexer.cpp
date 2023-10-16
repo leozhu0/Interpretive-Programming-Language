9dd71513715a47730bec357f44768548584fdaa0
John Lins <johnpaullins@gmail.com>
2023-10-15 22:32:38 -0700

    Don't sub line

--- src/lib/lexer.cpp
+++ src/lib/lexer.cpp
@@ -96,7 +96,7 @@ std::vector<Token> Lexer::lexer(){
     if(sequence.back().type == PARENTHESIS){
         sequence.push_back(Token{line-1,sequence.back().column + 1,"END", END});
     } else {
-        sequence.push_back(Token{line-1,1,"END", END});
+        sequence.push_back(Token{line,1,"END", END});
     }
     
 
