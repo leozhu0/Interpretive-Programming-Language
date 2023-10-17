f35b4dd58056a9d11759a85277fccd7124a80508
Leo Zhu <leozhu@ucsb.edu>
2023-10-16 19:10:37 -0700

    fixing final errors

--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -10,10 +10,10 @@ Parser::~Parser() {
 
 Parser::Parser(std::vector<Token> tokens) {
   //if (tokens.size() != 18 && tokens.size() != 7) {
-  //  for (Token& token : tokens) {
-  //    std::cout << token.token << std::endl;
-  //  }
-  //  std::cout << "_______________" << std::endl;
+    for (Token& token : tokens) {
+      std::cout << token.token << std::endl;
+    }
+    std::cout << "_______________" << std::endl;
   //}
 
   if (tokens.size() == 0) {
@@ -61,7 +61,7 @@ Node* Parser::createNode(std::vector<Token> tokens) {
     for (size_t i = start + 1; i < tokens.size(); ++i) {
       if (allowedParenthesis == 0 && i != tokens.size() - 1) {
         std::cout << "Unexpected token at line " << tokens[i].line << " column " << tokens[i].column << ": " << tokens[i].token << std::endl;
-	std::cout << "1";
+	//std::cout << "1";
 	exit(2);
       }
 
@@ -97,7 +97,7 @@ Node* Parser::createNode(std::vector<Token> tokens) {
 
 	  if (i == tokens.size()) {  
             std::cout << "Unexpected token at line " << tokens[i - 1].line << " column " << tokens[i - 1].column << ": " << tokens[i - 1].token << std::endl;
-	    std::cout << "2";
+	    //std::cout << "2";
 	    exit(2);
 	  }
 	}
@@ -114,7 +114,7 @@ Node* Parser::createNode(std::vector<Token> tokens) {
 
     if (allowedParenthesis != 0) {
       std::cout << "Unexpected token at line " << tokens[tokens.size() - 1].line << " column " << tokens[tokens.size() - 2].column << ": " << tokens[tokens.size() - 2].token << std::endl;
-      std::cout << "3";
+      //std::cout << "3";
       exit(2);
     }
 
