51e273487f8fabd15a3fbf4abadefe09230f28b8
John Lins <johnpaullins@gmail.com>
2023-10-18 14:47:50 -0700

    read char by char

--- src/lib/lexer.cpp
+++ src/lib/lexer.cpp
@@ -42,50 +42,69 @@ void Lexer::pushSeq(std::string element, TokenType type, int line, int column, s
 
 std::vector<Token> Lexer::lexer(){
     std::vector<Token> sequence;
-    std::string rawInput;
+    char rawInput;
     int line = 1;
-    while (std::getline(std::cin, rawInput)) {
-        //This will later be used to check for more than one decimal in number
-        int numDecimal = 0;
-        std::string element = "";
-        for(int i = 0; i < (int)rawInput.length(); i++){
-            TokenType type = tokenType(rawInput[i]);
-
-            if(type == NULLTYPE || (numDecimal > 0 && rawInput[i] == '.')){
-                std::cout << "Syntax error on line "<< line <<" column "<< i+1 <<"." << std::endl;
-                exit(1);
+    int i = 0;
+    int indents = 0;
+    //This will later be used to check for more than one decimal in number
+    int numDecimal = 0;
+    std::string element = "";
+    while (std::cin.get(rawInput)) {
+        if(rawInput == '\n'){
+            indents++;
+            if(indents > 1){
+                sequence.push_back(Token{line,1,"END", END});
+                return sequence;
+                //break;
             }
 
-            if(type != SPACE){
-                if(rawInput[i] == '.' || ((int)rawInput[i] >= 48 && (int)rawInput[i] <= 57)) {
-                    if(rawInput[i] == '.'){
-                        numDecimal++;
-                    }
-                    element += rawInput[i];
-                } else {
-                    pushSeq(element, NUMBER, line, i+1 - element.size(), sequence);
-                    numDecimal = 0;
-                    pushSeq(rawInput.substr(i, 1), type, line, i+1, sequence);
-                    element = "";
-                }
-
-            } else {
-                pushSeq(element, NUMBER, line, i+1 - element.size(), sequence);
-                element = "";
-                numDecimal = 0;
-            }
-
+            //When you start a newline, take what was inside element and add it to sequence. 
             //This will always be a number
-            if(i == (int)(rawInput.length()-1) && tokenType(element[0]) != NULLTYPE){ 
+            if(tokenType(element[0]) != NULLTYPE){ 
                 pushSeq(element, tokenType(element[0]), line, i+2 - element.size(), sequence);
             }
 
+
+            i=0;
+            line++;
+            element="";
+        } else {
+            indents = 0;
         }
-        element = "";
-        line++;
+
+        TokenType type = tokenType(rawInput);
+
+        if(type == NULLTYPE || (numDecimal > 0 && rawInput == '.')){
+            std::cout << "Syntax error on line "<< line <<" column "<< i+1 <<"." << std::endl;
+            exit(1);
+        }
+
+        if(type != SPACE){
+                if(rawInput == '.' || ((int)rawInput >= 48 && (int)rawInput <= 57)) {
+                    if(rawInput == '.'){
+                        numDecimal++;
+                    }
+                    element += rawInput;
+                } else {
+                    pushSeq(element, NUMBER, line, i+1 - element.size(), sequence);
+                    numDecimal = 0;
+                    std::string rawInputString(1, rawInput);
+                    pushSeq(rawInputString, type, line, i+1, sequence);
+                    element = "";
+                }
+
+        } else {
+            pushSeq(element, NUMBER, line, i+1 - element.size(), sequence);
+            element = "";
+            numDecimal = 0;
+        }
+
+        
+
+        i++;
     }
 
-    if(sequence.size() > 0 && sequence.back().type == PARENTHESIS){
+    if(sequence.size() > 0){
         sequence.push_back(Token{line-1,sequence.back().column + 1,"END", END});
     } else {
         sequence.push_back(Token{line,1,"END", END});
