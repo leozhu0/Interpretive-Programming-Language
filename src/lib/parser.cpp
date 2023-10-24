16a566d2379b6851921da8d37266f293c0bc6c36
Leo Zhu <leozhu@ucsb.edu>
2023-10-24 15:40:40 -0700

    finishing Infix Parser except for variable case

--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -249,19 +249,20 @@ Node* InfixParser::createTree(Node* leftHandSide, int minPrecedence, std::vector
   while (precedence(nextOp) >= minPrecedence) {
     std::string currOp = nextOp;
     
-    //TODO add variable case
-    NumNode* rightHandSide = new NumNode;
-    rightHandSide->value = nextToken(tokens).token;
+    Node* rightHandSide = nextNode(tokens);
 
     nextOp = peak(tokens).token;
 
-    //TODO add equivalent case with right-associative operator
-    while (precedence(nextOp) > precedence(currOp)) {
-      rightHandSide = createTree(rightHandSide, precedence(currOp) + 1, tokens);
+    while ((precedence(nextOp) > precedence(currOp)) || (nextOp == "=" && precedence(nextOp) == precedence(currOp))) {
+      int addedPrecedence = 1;
+      if (nextOp == "=") --addedPrecedence;
+
+      rightHandSide = createTree(rightHandSide, precedence(currOp) + addedPrecedence, tokens);
       nextOp = peak(tokens).token;
     }
 
     OpNode tempNode = new OpNode;
+    tempNode.value = currOp;
     tempNode->left = leftHandSide;
     tempNode->right = rightHandSide;
 
@@ -278,6 +279,8 @@ int InfixParser::precedence(std::string op) {
 
   else if (op == "*" || op == "/") return 2;
 
+  else if (op = "END" || op == ")") return -1;
+
   else {
     //TODO
     std::cout << "placeholder error 2" << std::endl;
@@ -287,25 +290,41 @@ int InfixParser::precedence(std::string op) {
 
 Token& InfixParser::peak(std::vector<Token> tokens) {
   for (size_t i = index; i < tokens.size(); ++i) {
-    if (tokens[i].type == OPERATOR) return tokens[i];
+    if (tokens[i].type == OPERATOR || tokens[i].token == ")") return tokens[i];
   }
 
   return tokens.back();
 }
 
-Token& InfixParser::nextToken(std::vector<Token> tokens) {
+Node* InfixParser::nextNode(std::vector<Token> tokens) {
   for (size_t i = index + 1; i < tokens.size(); ++i) {
-    if (tokens[i].type == NUMBER //TODO add variable case) {
+    if (tokens[i].type == NUMBER {
       index = i;
-      return tokens[i];
+
+      NumNode tempNode = new NumNode;
+      tempNode.value = token[i].token;
+
+      return tempNode;
     }
 
-    else if (token)
+    //TODO add variable case
+
+    else if (token[i].token == "(") {
+      index = i;
+      Node* tempNode = createTree();
+
+      if (token[index + 1].token != ")") {
+        std::cout << "placeholder error 4" << std:endl;
+	exit(2);
+      }
+
+      return tempNode;
+    }
 
   }
 
   //TODO
-  std::cout << "placeholder error 3" << std::end;
+  std::cout << "placeholder error 3" << std::endl;
   exit(2);
 }
 
