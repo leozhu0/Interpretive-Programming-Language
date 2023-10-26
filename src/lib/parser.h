da38f31ed5fd1bf5b28ceca5face293880d93a3e
John Lins <johnpaullins@gmail.com>
2023-10-25 23:59:08 -0700

    finished the structured data

--- src/lib/parser.h
+++ src/lib/parser.h
@@ -3,28 +3,19 @@
 #include "token.h"
 #include <map>
 
-enum NodeType {
-  NUMNODE,
-  OPNODE,
-  ASSIGNMENTNODE,
-  UNKNOWNNODE
-};
 struct Node {
   std::string value;
-  NodeType type = UNKNOWNNODE;
   virtual ~Node() {};
   virtual double getValue() = 0;
   virtual std::string toString() = 0;
 };
 
 struct NumNode : public Node {
-   NodeType type = NUMNODE;
   double getValue();
   std::string toString();
 };
 
 struct OpNode : public Node {
-   NodeType type = OPNODE;
   std::vector<Node*> children;
 
   ~OpNode();
@@ -33,7 +24,12 @@ struct OpNode : public Node {
 };
 
 struct AssignNode : public OpNode {
-   NodeType type = ASSIGNMENTNODE;
+  double getValue();
+};
+
+
+struct VarNode : public NumNode {
+  std::string varValue;
   double getValue();
 };
 
@@ -41,30 +37,10 @@ class Parser {
   Node* root;
 
   Node* createNode(std::vector<Token> tokens);
-
-  std::map<AssignNode*, double> variables;
-
-
+  std::map<std::string, VarNode*> variables;
 public:
   ~Parser();
   Parser(std::vector<Token> tokens);
-
-  std::string toString();
-  double calculate();
-};
-
-class InfixParser {
-  Node* root;
-  static size_t index = 0;
-
-  Node* createTree(Node* leftHandSide, int minPrecedence, std::vector<Token> tokens);
-  int precedence(std::string op);
-  Token& peak(std::vector<Token> tokens);
-  Node* nextNode(std::vector<Token> tokens);
-
-public:
-  InfixParser(std::vector<Token> tokens);
-  ~InfixParser();
 
   std::string toString();
   double calculate();
