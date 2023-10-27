6d6ac4324172a0d306d1b65d35f7d96fcdac8b2e
John Lins <johnpaullins@gmail.com>
2023-10-26 22:50:57 -0700

    Single line lexer with vars

--- src/lex.cpp
+++ src/lex.cpp
@@ -12,5 +12,8 @@ int main() {
     for(int i = 0; i < (int)(seq.size()); i++){
         std::cout << std::right << std::setw(4) << seq.at(i).line << std::right << std::setw(5) << seq.at(i).column << "  " << seq.at(i).token << std::endl;
     }
+
+
+   
     return 0;
 }
