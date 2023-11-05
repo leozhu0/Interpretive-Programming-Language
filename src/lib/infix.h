f3fa3f11f8cf007c55eaf29e790093f6e365a217
Leo Zhu <leozhu@ucsb.edu>
2023-11-04 23:53:36 -0700

    trying to fix warning

--- src/lib/infix.h
+++ src/lib/infix.h
@@ -4,28 +4,6 @@
 #include "token.h"
 // #include "node.h"
 
-class InfixParser {
-  Node* root;
-  int index = -1;
-  size_t parenNum = 0;
-  bool updateVariables = true;
-  std::vector<std::pair<std::string, Node*>> variableBuffer;
-
-  Node* createTree(Node* leftHandSide, int minPrecedence, std::vector<Token> tokens);
-  int precedence(std::string op);
-  Token& peak(std::vector<Token> tokens);
-  Node* nextNode(std::vector<Token> tokens);
-
-public:
-  InfixParser(std::vector<Token> tokens);
-  ~InfixParser();
-
-  std::string toString();
-  double calculate();
-};
-
-//_____________________________________________________________________________
-
 struct Node {
   std::string value;
   const TokenType returnType;
@@ -77,4 +55,26 @@ struct LogicNode : public OpNode {
   LogicNode() : OpNode(BOOL) {}
 
   double getValue();
+};
+
+//______________________________________________________________________________
+
+class InfixParser {
+  Node* root;
+  int index = -1;
+  size_t parenNum = 0;
+  bool updateVariables = true;
+  std::vector<std::pair<std::string, Node*>> variableBuffer;
+
+  Node* createTree(Node* leftHandSide, int minPrecedence, std::vector<Token> tokens);
+  int precedence(std::string op);
+  Token& peak(std::vector<Token> tokens);
+  Node* nextNode(std::vector<Token> tokens);
+
+public:
+  InfixParser(std::vector<Token> tokens);
+  ~InfixParser();
+
+  std::string toString();
+  double calculate();
 };
