177cafc435aeb7fa876312ca72dbdd1b49c9d4cb
John Lins <johnpaullins@gmail.com>
2023-11-05 21:53:48 -0800

    All lex errors fixed

--- src/lex.cpp
+++ src/lex.cpp
@@ -3,14 +3,14 @@
 #include <iomanip>
 #include <vector>
 #include <fstream>
-#include "lib/lexer.h"
+#include "lib/lexer.cpp"
 
 int main() {
     try{
         Lexer lexer = Lexer();
         std::vector<Token> seq= lexer.lexer();
         for(int i = 0; i < (int)(seq.size()); i++){
-            std::cout << std::right << std::setw(4) << seq.at(i).line << std::right << std::setw(5) << seq.at(i).column << "  " << seq.at(i).token  <<std::endl;
+            std::cout << std::right << std::setw(4) << seq.at(i).line << std::right << std::setw(5) << seq.at(i).column << "  " << seq.at(i).token <<std::endl;
         }
     } catch (const std::exception& e) {
       std::cout << e.what() << std::endl;
