c72237cc3d0cb90fa3c5a28771484a147b286f94
John Lins <johnpaullins@gmail.com>
2023-11-18 15:28:24 -0800

    Divide by semicolon

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -1,5 +1,5 @@
-#include "lib/lexer.h" // cpp
-#include "lib/infix.h" //cpp
+#include "lib/lexer.cpp" // cpp
+#include "lib/infix.cpp" //cpp
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -38,7 +38,7 @@ double condToDouble(std::string raw){
     return stod(raw);
 }
 
-std::string EvaluateExpression(std::vector<Token> tokens){
+std::string evaluateExpression(std::vector<Token> tokens){
     //Return nothing if tokens vector is empty or just END
     if((int)tokens.size() == 0 || tokens.at(0).type == END){
         return "";
@@ -54,7 +54,7 @@ std::string EvaluateExpression(std::vector<Token> tokens){
 }
 
 
-void EvaluateExpressionChunk(std::vector<Token> tokens){
+void evaluateExpressionChunk(std::vector<Token> tokens){
     if(tokens.size() == 0){
         return;
     }
@@ -62,8 +62,19 @@ void EvaluateExpressionChunk(std::vector<Token> tokens){
     
     int index = 0; 
 
+
+    for(int i = 0; i < (int)tokens.size(); i++){
+        std::vector<Token> tempRow;
+        while(i < (int)tokens.size() && tokens[i].token != ";"){
+            tempRow.push_back(tokens[i]);
+            i++;
+        }
+        multilineTokens.push_back(tempRow);
+    }
+
+
     //If END is on a new line, then stop at the line before, otherwise do the whole thing
-    for(int i = 0; i < tokens[(int)tokens.size() - 1 - ((tokens[(int)tokens.size()-1].column == 1)?1:0)].line; i++){
+    /*for(int i = 0; i < tokens[(int)tokens.size() - 1 - ((tokens[(int)tokens.size()-1].column == 1)?1:0)].line; i++){
         std::vector<Token> tempRow;
         
         while((int)tokens.size() > index && tokens[index].line == i+1){
@@ -71,15 +82,15 @@ void EvaluateExpressionChunk(std::vector<Token> tokens){
             index++; 
         }
         multilineTokens.push_back(tempRow);
-    }
+    }*/
 
     for (const auto &line : multilineTokens)
     {
-        EvaluateExpression(line);
+        evaluateExpression(line);
     }
 }
 
-void ParseBlock(std::vector<Token>& tokens) {
+void parseBlock(std::vector<Token>& tokens) {
     //The last token must always have an end (to know when to terminate later)
     if(tokens.back().type != END){
         tokens.push_back(Token{tokens.back().line, tokens.back().column+1,"END", END});
@@ -105,7 +116,7 @@ void ParseBlock(std::vector<Token>& tokens) {
                 i++;
             }
 
-            std::cout << EvaluateExpression(printExpr)<< std::endl;
+            std::cout << evaluateExpression(printExpr)<< std::endl;
             i++;
         }
 
@@ -127,9 +138,9 @@ void ParseBlock(std::vector<Token>& tokens) {
 
             int blockEnd = i;
             std::vector<Token> conditionExpr(tokens.begin() + conditionStart, tokens.begin() + conditionEnd);
-            while (condToDouble(EvaluateExpression(conditionExpr)) != 0) {
+            while (condToDouble(evaluateExpression(conditionExpr)) != 0) {
                 std::vector<Token> block(tokens.begin() + blockStart, tokens.begin() + blockEnd);
-                ParseBlock(block);
+                parseBlock(block);
             }
            
         } 
@@ -149,15 +160,45 @@ void ParseBlock(std::vector<Token>& tokens) {
                 else if (tokens[i].token == "}") blockParen--;
             }
             
-            if (condToDouble(EvaluateExpression(conditionExpr)) != 0) {
+            if (condToDouble(evaluateExpression(conditionExpr)) != 0) {
                 std::vector<Token> block(tokens.begin() + blockStart, tokens.begin() + i); 
-                ParseBlock(block);
+                parseBlock(block);
                 prevCond = true;
             } else{
                 prevCond = false;
             } 
             i++;
         } 
+
+        /*else if (tokens[i].token == "def") {
+            int inputStart = i + 1;
+            std::vector<Token> arguments;
+            bool argIndex = true;
+            while (tokens[i].token != "{") {
+                if(argIndex){
+                    arguments.push_back(tokens[i]);
+                    argIndex = false;
+                } if(tokens[i].token == ","){
+                    argIndex = true;
+                }
+                i++;
+            }
+            
+            std::vector<Token> inputExpr(tokens.begin() + inputStart, tokens.begin() + i);
+            int blockStart = i + 1;
+            int blockParen = 1;
+            while (blockParen > 0) {
+                i++;
+                if (tokens[i].token == "{") blockParen++;
+                else if (tokens[i].token == "}") blockParen--;
+            }
+            
+            
+            std::vector<Token> block(tokens.begin() + blockStart, tokens.begin() + i); 
+            STOREFUNCTIONINMAP(arguments, block);
+          
+            i++;
+        } */
 
         else if (tokens[i].token == "else if") {
             int conditionStart = i + 1;
@@ -175,9 +216,9 @@ void ParseBlock(std::vector<Token>& tokens) {
                 else if (tokens[i].token == "}") blockParen--;
             }
 
-            if(prevCond == false && condToDouble(EvaluateExpression(conditionExpr)) != 0){
+            if(prevCond == false && condToDouble(evaluateExpression(conditionExpr)) != 0){
                 std::vector<Token> block(tokens.begin() + blockStart, tokens.begin() + i);
-                ParseBlock(block);
+                parseBlock(block);
                 prevCond = true;
             }
             i++;
@@ -196,7 +237,7 @@ void ParseBlock(std::vector<Token>& tokens) {
             
             if(prevCond == false){
                 std::vector<Token> block(tokens.begin() + blockStart, tokens.begin() + i);
-                ParseBlock(block);
+                parseBlock(block);
             }
             prevCond = true;
         } 
@@ -221,7 +262,7 @@ void ParseBlock(std::vector<Token>& tokens) {
             std::vector<Token> block (tokens.begin() + blockStart, tokens.begin() + blockEnd);
             
             if((int)block.size() > 0 && block.at(0).type != END){
-                EvaluateExpressionChunk(block);
+                evaluateExpressionChunk(block);
             }
             
         }
@@ -242,7 +283,7 @@ int main() {
     }
 
     try {
-        ParseBlock(tokens);
+        parseBlock(tokens);
     } 
     catch (const std::exception& e) {
       std::cout << e.what() << std::endl;
