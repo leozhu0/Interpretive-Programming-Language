d569619f253ff800112712637825aeffb2c07ec4
Leo Zhu <leozhu@ucsb.edu>
2023-11-21 18:21:33 -0800

    fixed error regarding array assignment

--- src/lib/infix.cpp
+++ src/lib/infix.cpp
@@ -733,7 +733,7 @@ std::string OpNode::toString() {
 
 Value AssignNode::getValue([[maybe_unused]] std::map<std::string, Value>& variables) {
   if (!(lhs->isVar)) {
-    if (isValidArrayAssignment) return rhs->getValue(variables);
+    if (lhs->isValidArrayAssignment) return rhs->getValue(variables);
 
     std::ostringstream error;
     error << "Runtime error: invalid assignee.";
