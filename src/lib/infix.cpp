f7b6b501de9662ef77bd76074b281b7397a85884
Leo Zhu <leozhu@ucsb.edu>
2023-11-21 18:28:11 -0800

    fixed error regarding array assignment

--- src/lib/infix.cpp
+++ src/lib/infix.cpp
@@ -733,7 +733,7 @@ std::string OpNode::toString() {
 
 Value AssignNode::getValue([[maybe_unused]] std::map<std::string, Value>& variables) {
   if (!(lhs->isVar)) {
-    if (lookup != nullptr) return rhs->getValue(variables);
+    if (lookUp != nullptr) return rhs->getValue(variables);
     //if (lhs->isValidArrayAssignment) return rhs->getValue(variables);
 
     std::ostringstream error;
