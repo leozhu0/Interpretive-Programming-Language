02188be090a72bbfa6ad6d9cc8d004cb8bc3ce63
Leo Zhu <leozhu@ucsb.edu>
2023-11-22 16:29:08 -0800

    fixed an error that arose from fixing another one

--- src/calc.cpp
+++ src/calc.cpp
@@ -14,17 +14,6 @@ int main() {
 
   while (std::getline(std::cin, line)) {
     try {
-      std::vector<Token> tempTokens = lexer.lexer(line);
-
-      std::cout << "___start___" << std::endl;
-      for (Token token : tempTokens)  {
-        std::cout << "token: " << token.token << std::endl;
-	std::cout << "type " << token.type << std::endl;
-	std::cout << "line: " << token.line << std::endl;
-	std::cout << "column: " << token.column << std::endl << std::endl;
-      }
-      std::cout << "___end___" << std::endl;
-
       InfixParser infixParser = InfixParser(lexer.lexer(line), variables);
       std::cout << infixParser.toString() << std::endl << infixParser.calculate() << std::endl;
     }
